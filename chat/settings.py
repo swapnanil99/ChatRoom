963b4e0998aa4610c42b934950d09cc45d244d60
Swapnanil Maity <swapnanilmaity99@gmail.com>
2025-11-19 14:55:12 +0530

    Use InMemoryChannelLayer when REDIS_URL is missing

--- chat/settings.py
+++ chat/settings.py
@@ -1,11 +1,6 @@
 import os
 from pathlib import Path
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-=b^(d#dz_^traixp$(g8l$1rux@sx6zb2!jy94x2iy)0+)*p@7'
@@ -13,7 +8,7 @@ SECRET_KEY = 'django-insecure-=b^(d#dz_^traixp$(g8l$1rux@sx6zb2!jy94x2iy)0+)*p@7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["chatroom-axkr.onrender.com",  # Render er URL
+ALLOWED_HOSTS = ["chatroom-axkr.onrender.com",  # Render URL
     "127.0.0.1",
     "localhost",]
 
