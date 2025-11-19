bdf013bb306f1bff3b341317f416e1f93969e16a
Swapnanil Maity <swapnanilmaity99@gmail.com>
2025-11-19 16:15:44 +0530

    Customize Django admin branding

--- chat/settings.py
+++ chat/settings.py
@@ -49,7 +49,7 @@ ROOT_URLCONF = 'chat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -151,6 +151,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- room/admin.py
+++ room/admin.py
@@ -7,3 +7,8 @@ class ChatMessageAdmin(admin.ModelAdmin):
     list_display = ("id", "room_name", "username", "message", "created_at")
     list_filter = ("room_name", "username")
     search_fields = ("room_name", "username", "message")
+
+
+admin.site.site_header = "ChatRoom Command Center"
+admin.site.site_title = "ChatRoom Admin"
+admin.site.index_title = "Realtime Messaging Dashboard"
